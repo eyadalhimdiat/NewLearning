7ef79599ecfff43e67156e862bef97dacf864538
ehimdi486 <eyadai_himdiat@comcast.com>
2019-05-09 23:36:45 -0400

    dsds

--- first_file.py
+++ first_file.py
@@ -1,6 +1,8 @@
 import numpy
 from numpy import array
-
+a = 5
+b = 'fffffffffff'
+c = [1,2,4]
 first_array = array([1,2,3,4,5,6,7,8,9])
 x = first_array[:5]
 print(x)
--- second_file.py
+++ second_file.py
@@ -1,7 +1,7 @@
 import numpy
 from numpy import array
 a = dict()
-b = ''
-c = []
+b = 'sdads6,6'
+c = [4,5,6]
 second_array = array([1,2,3,4,5,6,7,8,9])
 print(second_array)
